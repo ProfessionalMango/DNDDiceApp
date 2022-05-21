e78ca8d243b6458a953a31c80045f65e5ebfde58
bryanbarreto <bryan94barreto@gmail.com>
2022-05-21 17:44:29 -0400

    layout

--- app/src/main/java/dnddiceapp/App.java
+++ app/src/main/java/dnddiceapp/App.java
@@ -5,23 +5,55 @@ package dnddiceapp;
 import java.awt.*; 
 import javax.swing.*;
 
+import org.checkerframework.checker.units.qual.radians;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
     private static void printWindow() {
         JFrame window = new JFrame("First Window");
         JLabel text = new JLabel("WOW! A Label!", SwingConstants.CENTER);
-        text.setPreferredSize(new Dimension(300, 100));
-        window.getContentPane().add(text, BorderLayout.CENTER);
+        JPanel panel = new JPanel();
+        GroupLayout layout = new GroupLayout(panel);
+        JButton roll = new JButton("ROLL");
+        JButton clear = new JButton("CLEAR");
+        JTextField input = new JTextField();
+        JTextArea recentRoll = new JTextArea();
+        JTextArea pastRolls = new JTextArea();
+
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+        layout.setVerticalGroup(layout.createSequentialGroup()
+            .addComponent(input)
+            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addComponent(recentRoll)
+                .addComponent(pastRolls))
+            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addComponent(roll)
+                .addComponent(clear)
+            )
+        );
+        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addComponent(input)
+                .addGroup(layout.createSequentialGroup()
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(recentRoll)
+                        .addComponent(roll))
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(pastRolls)
+                        .addComponent(clear)))
+        );
+        panel.setLayout(layout);
+        panel.setPreferredSize(new Dimension(300, 100));
+
+
+        window.getContentPane().add(panel, BorderLayout.CENTER);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.pack();
         window.setVisible(true);
     }
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
         printWindow();
     }
+
 }
 
